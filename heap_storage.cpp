c6593faf6919d2fb856a5846e6bbc6b10f21c546
sonalidesarda <39871860+sonalidesarda@users.noreply.github.com>
2022-04-24 14:19:57 -0700

    Update heap_storage.cpp

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -237,7 +237,7 @@ void HeapFile::db_open(uint flags) {
 void HeapFile::put(DbBlock *block) {
     BlockID  blockId = block->get_block_id();
     Dbt block(&blockId,sizeof(blockId));
-    this->db.put(nullptr, &block, block->get_block(),0);
+    this->db.put(nullptr, block, block->get_block(),0);
 }
 
 
