40ce92ae82b36a3ae0afb9d259e1639d020006c7
sonalidesarda <39871860+sonalidesarda@users.noreply.github.com>
2022-04-24 00:16:20 -0700

    Update heap_storage.cpp

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -195,8 +195,8 @@ void HeapFile::create() {
 }
 
 void HeapFile::close() {
-    db.close(0);
-    closed = true;
+    this->db.close(0);
+    this->closed = true;
 }
 
 BlockIDs *HeapFile::block_ids() {
